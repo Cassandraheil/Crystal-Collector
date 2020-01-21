19a698e797fa6e21f887e780863ebfb29e70b1fe
Cassandra Heil <Cassandraheil19@gmail.com>
2020-01-21 18:17:17 -0500

    got random numbers assigned to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,68 @@
 $(document).ready(function(){
-var wins = $("#wins")
-var losses = $("#losses")
+    var counter = 0
+    var losses = 0
+    var wins = 0 
+    $("#losses").html("Losses: "+ losses)
+    $("#wins").html("Wins: "+ wins)
+
+    
+    var goalNumber = Math.floor(Math.random()* 101 + 1) 
+            $("#goalNumber").text(goalNumber + 19);
+            console.log(goalNumber)
 
 
-var goalNumber = 55
-$("#goalNumber").text(goalNumber);
-var counter = 0
+        var randomNumber = Math.round(Math.random()* 11 + 1);
+        
+        for (i = 0; i < 5; i++){  
+                var crystal = $("<img>");
+                crystal.addClass("crystal");
+                crystal.attr("src", "https://images-na.ssl-images-amazon.com/images/I/41V8nsnhKVL._SX425_.jpg"); //possibly get differrent imagaes from folder instead of internet
+                crystal.attr("data-value", randomNumber);
+                randomNumber = Math.round(Math.random()* 11 + 1)
 
-var numberArray = [10, 2, 3, 7, 1];
-// var randomNumber = numberArray[Math.round(Math.random())];
-console.log(counter)
-for (i = 0; i <  numberArray.length; i++){  
-        var crystal = $("<img>");
-        crystal.addClass("crystal");
-        crystal.attr("src", "https://images-na.ssl-images-amazon.com/images/I/41V8nsnhKVL._SX425_.jpg")
-        crystal.attr("data-value", numberArray[i]);
-        $("#crystals").append(crystal)    
- }
 
-$(".crystal").on("click", function(){
-    var crystalValue = ($(this).attr("data-value"));
-    crystalValue = parseInt(crystalValue);
-    console.log(this)
+                $("#crystals").append(crystal); 
+        }
 
-        counter += crystalValue;
+    
+
+    $(".crystal").on("click", function(){
+        var crystalValue = ($(this).attr("data-value"));
+        crystalValue = parseInt(crystalValue);
         
 
-        alert("your new score is " + counter )
-        if (counter === goalNumber){
-            alert("you win!");
+            counter += crystalValue;
+        
+
+            alert("your new score is " + counter )
+            if (counter === goalNumber + 19){
+                alert("you win!");
+                wins ++;
+                $("#wins").html("Wins: "+ wins)
+                reset()
+            }
+            else if (counter >= goalNumber + 19){
+                alert("you lose")
+                losses ++;
+                $("#losses").html("Losses: "+ losses) 
+            reset()
+            }        
+    });
+
+function reset(){
+        var goalNumber = Math.floor(Math.random()* 101) 
+            $("#goalNumber").text(goalNumber + 19);
+            console.log("this just reset")
+            counter =0
+
+        var numberArray = [12, 1, 4, 8, 9];
+        
+        for (i = 0; i <  numberArray.length; i++){  
+                crystal.attr("data-value", numberArray[i]);
+                $("#crystals").append(crystal)    
         }
-        else if (counter >= goalNumber){
-            alert("you loose")
-        }
-});
+    }
 
-
-
-
-// for (i = 0; i <  buttons.length; i++){
-
-// }
+        
 
 })
