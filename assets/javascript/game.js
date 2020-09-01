cf0ebd3e86398ba8609ec346080ae0e126dd9439
Cassandra Heil <Cassandraheil19@gmail.com>
2020-09-01 18:32:56 -0400

    buttons now provide easier and harder options

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,47 @@ $(document).ready(function(){
     var counter = 0
     var losses = 0
     var wins = 0 
-    var number = 4
 
     $("#losses").html("Losses: "+ losses)
     $("#wins").html("Wins: "+ wins)
 
 
-    $("#easyBtn").on("click", easyMode);
-    $("#hardBtn").on("click", hardMode);
+    $("#easyBtn").click(function(){
+        $("#crystals").empty();
+        easyMode()
+    });
+    $("#hardBtn").click(function(){
+        $("#crystals").empty();
+        hardMode()
+    });
 
     function easyMode(){
         console.log("easy mode bitch")
-        number = 4
+        
+
+        for (i = 0; i < 4; i++){  
+            var crystal = $("<img>");
+            crystal.addClass("crystal");
+            crystal.attr("src", "https://images-na.ssl-images-amazon.com/images/I/41V8nsnhKVL._SX425_.jpg"); 
+                    
+            crystal.attr("data-value", randomNumber);
+            randomNumber = Math.round(Math.random()* 11 + 1)
+    
+            $("#crystals").append(crystal); 
+            }
     }
+
     function hardMode(){
-        number = 8
-        console.log("this shit be haaaaard")
-    }
+        for (i = 0; i < 8; i++){  
+            var crystal = $("<img>");
+            crystal.addClass("crystal");
+            crystal.attr("src", "https://images-na.ssl-images-amazon.com/images/I/41V8nsnhKVL._SX425_.jpg"); 
+                    
+            crystal.attr("data-value", randomNumber);
+            randomNumber = Math.round(Math.random()* 11 + 1)
+    
+            $("#crystals").append(crystal); 
+            }    }
     
     var goalNumber = Math.floor(Math.random()* 101 + 1) 
             $("#goalNumber").text(goalNumber + 19);
@@ -28,16 +52,14 @@ $(document).ready(function(){
     var randomNumber = Math.round(Math.random()* 11 + 1);
 
         
-    for (i = 0; i < number; i++){  
-        var crystal = $("<img>");
-        crystal.addClass("crystal");
-        crystal.attr("src", "https://images-na.ssl-images-amazon.com/images/I/41V8nsnhKVL._SX425_.jpg"); 
-                
-        crystal.attr("data-value", randomNumber);
-        randomNumber = Math.round(Math.random()* 11 + 1)
-
-        $("#crystals").append(crystal); 
-        }
+    // for (i = 0; i < number; i++){  
+    //     var crystal = $("<img>");
+    //     crystal.addClass("crystal");
+    //     crystal.attr("src", "https://images-na.ssl-images-amazon.com/images/I/41V8nsnhKVL._SX425_.jpg");         
+    //     crystal.attr("data-value", randomNumber);
+    //     randomNumber = Math.round(Math.random()* 11 + 1)
+    //     $("#crystals").append(crystal); 
+    //     }
 
 
     
